0f99ce8989a32b54acb08ab0e7c791fa11f1772a
Cristian Poll <git@cristianpoll.com>
2026-04-05 15:25:27 -0400

    add player join menu

---
+++ src/globals/players.gd
@@ -0,0 +1,28 @@
+extends Node
+
+class Player extends RefCounted:
+    var device_number = 10
+    var player_number = 10
+    
+    func _init(device_number: int) -> void:
+        self.device_number = device_number
+        
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+    pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+    pass
+
+# TODO: Manage joined players across scenes.
+# join_game_menu adds/removes players, this singleton won't handle itself
+# game loop loops through players and gets each player's device?
+# Alternatively, use this class to get the device and pass it to the player we instantiate
+
+func add_player(device_number: int) -> Player:
+    return Player.new(device_number)
+    
+func remove_player(player_number: int) -> void:
+    pass
+    
---
+++ src/main_menu/join_game_menu.gd
@@ -0,0 +1,80 @@
+extends Control
+
+# This menu handles all inputs by looping through joined/unjoined devices.
+# When start() is called, it populates the Players global and changes scenes.
+
+# Joined devices; their position in the array corresponds with P1-P4
+var joined_devices = [null, null, null, null]
+var countdown = null
+
+@onready var center_label = %CenterLabel
+@onready var p1_label = $Layout/P1P2/P1/PData
+@onready var p2_label = $Layout/P1P2/P2/PData
+@onready var p3_label = $Layout/P3P4/P3/PData
+@onready var p4_label = $Layout/P3P4/P4/PData
+var p_labels
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+    p_labels = [p1_label, p2_label, p3_label, p4_label]
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+    handle_input()
+    update_ui()
+    
+    if countdown != null:
+        countdown -= delta
+        if countdown <= 0:
+            start_game()
+
+func handle_input():
+    var devices = Input.get_connected_joypads()
+    var unjoined_devices = devices.filter(func(d): return d not in joined_devices)
+
+    # Handle unjoined device input
+    for device in unjoined_devices:
+        if MultiplayerInput.is_action_just_pressed(device, "Confirm"):
+            # Unjoined device wants to join
+            var first_empty_player_slot = joined_devices.find(null)
+            if first_empty_player_slot > -1:
+                joined_devices[first_empty_player_slot] = device
+    
+    # Handle joined device input
+    for i in joined_devices.size():
+        var device = joined_devices[i]
+        if device == null:
+            continue
+            
+        if MultiplayerInput.is_action_just_pressed(device, "Back"):
+            joined_devices[i] = null
+            stop_countdown()
+        elif MultiplayerInput.is_action_just_pressed(device, "Start"):
+            start_countdown()
+
+func start_countdown():
+    countdown = 5
+    
+func stop_countdown():
+    countdown = null
+    
+func start_game():
+    # TODO: Add players/devices to Players
+    print("start game")
+    pass
+    
+func update_ui():
+    for i in joined_devices.size():
+        if joined_devices[i] == null:
+            p_labels[i].text = "Press Confirm to Join"
+        else:
+            p_labels[i].text = "Joined!"
+        
+    if countdown != null:
+        center_label.text = str(int(ceil(countdown)))
+    else:
+        # TODO: Change the label depending on whether 2+ players have joined
+        center_label.text = "Press any key to join"
+        
+        
