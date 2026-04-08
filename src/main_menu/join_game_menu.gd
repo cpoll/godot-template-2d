fa9ea9e0218cb083e4d6f9ea57387bf2e4ef482a
Cristian Poll <git@cristianpoll.com>
2026-04-07 20:45:40 -0400

    refactor

--- src/main_menu/join_game_menu.gd
+++ src/main_menu/join_game_menu.gd
@@ -3,8 +3,17 @@ extends Control
 # This menu handles all inputs by looping through joined/unjoined devices.
 # When start() is called, it populates the Players global and changes scenes.
 
+class Device extends RefCounted:
+    var device_id = null
+    var device_name = ""
+
 # Joined devices; their position in the array corresponds with P1-P4
-var joined_devices = [null, null, null, null]
+var joined_devices = [
+    Device.new(),
+    Device.new(),
+    Device.new(),
+    Device.new()
+]
 var countdown = null
 
 @onready var center_label = %CenterLabel
@@ -31,24 +40,27 @@ func _process(delta: float) -> void:
 
 func handle_input():
     var devices = Input.get_connected_joypads()
-    var unjoined_devices = devices.filter(func(d): return d not in joined_devices)
+    var unjoined_devices = devices.filter(func(d): return d not in joined_devices.map(func(d): return d.device_id))
 
     # Handle unjoined device input
     for device in unjoined_devices:
         if MultiplayerInput.is_action_just_pressed(device, "Confirm"):
             # Unjoined device wants to join
-            var first_empty_player_slot = joined_devices.find(null)
+            
+            var first_empty_player_slot = joined_devices.find_custom(func(d): return d.device_id == null)
             if first_empty_player_slot > -1:
-                joined_devices[first_empty_player_slot] = device
+                joined_devices[first_empty_player_slot].device_id = device
+                joined_devices[first_empty_player_slot].device_name = Input.get_joy_name(device) # TODO: This isn't working. get_joy_info doesn't give much either
     
     # Handle joined device input
     for i in joined_devices.size():
-        var device = joined_devices[i]
+        var device = joined_devices[i].device_id
         if device == null:
             continue
             
         if MultiplayerInput.is_action_just_pressed(device, "Back"):
-            joined_devices[i] = null
+            joined_devices[i].device_id = null
+            joined_devices[i].device_name = ""
             stop_countdown()
         elif MultiplayerInput.is_action_just_pressed(device, "Start"):
             start_countdown()
@@ -66,10 +78,10 @@ func start_game():
     
 func update_ui():
     for i in joined_devices.size():
-        if joined_devices[i] == null:
+        if joined_devices[i].device_id == null:
             p_labels[i].text = "Press Confirm to Join"
         else:
-            p_labels[i].text = "Joined!"
+            p_labels[i].text = "Joined!" + joined_devices[i].device_name
         
     if countdown != null:
         center_label.text = str(int(ceil(countdown)))
