983f6ea42d729c497a1f5cd20e1c11b366e9b994
Cristian Poll <git@cristianpoll.com>
2026-04-10 19:45:44 -0400

    add debug keyboard

--- src/game/game.gd
+++ src/game/game.gd
@@ -4,6 +4,9 @@ var character_scene = preload("res://src/game/character.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+    if Globals.DEBUG:
+        Players.add_player(-1)
+    
     for player in Players.players:
         var c = character_scene.instantiate()
         c.init(DeviceInput.new(player.device_number))
