4cf7d528d8e0ce8af40a595eb20b757f66efb8fd
Cristian Poll <git@cristianpoll.com>
2026-04-10 20:38:40 -0400

    add ripple shader

--- src/game/game.gd
+++ src/game/game.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var character_scene = preload("res://src/game/character.tscn")
+@onready var players_container = $PlayersContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,7 +11,7 @@ func _ready() -> void:
     for player in Players.players:
         var c = character_scene.instantiate()
         c.init(DeviceInput.new(player.device_number))
-        add_child(c)
+        players_container.add_child(c)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
